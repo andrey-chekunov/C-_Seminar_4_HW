6e43139ceb1f799abe418aed0875d4470355f926
andrey-chekunov <79117325352@yandex.ru>
2022-06-18 16:50:27 +0300

    Resolve task 29 фигня какая-то

---
+++ Task_29_HW/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Вывод сделать отдельным методом.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] array = new int[8];
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ,");
+    }
+   
+}
+//RandomArray(array);
+RandomArray(array);
+PrintArray(array);
+
+
+
+
+
+
+
+
+
+
+
+
